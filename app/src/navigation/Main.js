a4a58421476304400c3e3b8145b78161a7095267
Iberi Daniel <danieliberi99@gmail.com>
2022-05-25 20:54:04 +0100

    landing page not completed. Check landingpage2

--- app/src/navigation/Main.js
+++ app/src/navigation/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import HeroNavBar from '../components/HeroNavBar';
-import LandingPage from '../pages/LandingPage';
+// import LandingPage from '../pages/LandingPage';
 import Login from '../pages/Login';
 import Profile from '../pages/Profile';
 import Profile2 from '../pages/Profile2';
@@ -12,11 +12,10 @@ const Main = () => {
       <div className='bg-slate-100 h-full'>
           <BrowserRouter>
               <Routes>
-                  <Route path='/' element={<LandingPage />}/>
+                  <Route path='/' element={<LandingPage2 />}/>
                   <Route path='/login' element={<Login />}/>
                   <Route path='/profilepage' element={<Profile />}/>
                   <Route path='/profile' element={<Profile2 />} />
-                  <Route path='/landingpage2' element={<LandingPage2 />}/>
               </Routes>
           </BrowserRouter>
     </div>
--- app/src/pages/LandingPage2.js
+++ app/src/pages/LandingPage2.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Body from '../components/LandingPage/Body';
-import HeroNavBar from '../components/LandingPage/HeroNavBar';
+import Body from '../components/LandingPage2/Body';
+import HeroNavBar from '../components/LandingPage2/HeroNavBar';
 
 const LandingPage2 = () => {
     return (
