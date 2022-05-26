48d4f9f308b9fe6e8898c724dedebbedef47470e
Bamgbose Aderounmu <aderounmubamgbose@yahoo.com>
2022-05-26 15:20:29 +0100

    mobile added

--- app/src/components/UserGroup/UserMember.js
+++ app/src/components/UserGroup/UserMember.js
@@ -7,13 +7,13 @@ const UserMember = () => {
         <div className="text-xl font-medium">
             afsgsgggsggsgggszzbbzb
         </div>
-        <div className="text-xl">
+        <div className="text-base lg:text-xl">
             12/10/2031
         </div>
-        <div className="text-xl">
+        <div className="text-base lg:text-xl">
             ETH 1000
         </div>
-        <div className='flex flex-row gap-6'>
+        <div className='hidden lg:flex flex-row gap-6'>
             <AiFillCheckCircle className="text-green-600 text-2xl"/>
             <AiFillCloseCircle className='text-red-600 text-2xl'/>
             <AiFillCheckCircle className="text-green-600 text-2xl"/>
